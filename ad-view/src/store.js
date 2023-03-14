79958fe32b95af586c33b50ae5fe734b54412c61
Tejash <tejash.jl1020@gmail.com>
2023-03-14 18:43:29 +0530

    added requirements file

--- ad-view/src/store.js
+++ ad-view/src/store.js
@@ -12,18 +12,23 @@ const sendMessage = (message) => {
 }
 
 const subscribeWs = (callback) => {
-    socket = new WebSocket('ws://localhost:5000/echo');
+    try {
+        socket = new WebSocket('ws://127.0.0.1:5000/echo');
 
 // Connection opened
-    socket.addEventListener('open', function (event) {
-        console.log("It's open");
-    });
+        socket.addEventListener('open', function (event) {
+            console.log("It's open");
+        });
 
 // Listen for messages
-    socket.addEventListener('message', function (event) {
-        messageStore.set(event.data);
-    });
-    messageStore.subscribe(callback);
+        socket.addEventListener('message', function (event) {
+            messageStore.set(event.data);
+        });
+        messageStore.subscribe(callback);
+    } catch (e) {
+        debugger
+        console.log(e)
+    }
 }
 
 
